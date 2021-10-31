55848489dceb6e8018ccd7d528d3cf1236002f60
Justin Jang <jangju99@gmail.com>
2021-10-31 12:46:57 -0700

    vscode single quote setting

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,31 @@
-const express = require("express");
+const express = require('express');
 
-const cors = require("cors");
-const morgan = require("morgan");
-const path = require("path");
-require("dotenv").config();
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-const NODE_ENV = process.env.NODE_ENV || "development";
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const initializeExpress = () => {
   const app = express();
 
-  if (NODE_ENV === "production") {
+  if (NODE_ENV === 'production') {
     app.use(cors);
-    app.use(morgan("combined"));
+    app.use(morgan('combined'));
   } else {
-    app.use(morgan("dev"));
+    app.use(morgan('dev'));
   }
 
-  app.use("/", express.static(path.resolve(__dirname, "../public")));
+  app.use('/', express.static(path.resolve(__dirname, '../public')));
 
-  app.get("*", (req, res) =>
-    res.sendFile("index.html", { root: path.join("__dirname", "../public") })
+  app.get('*', (req, res) =>
+    res.sendFile('index.html', { root: path.join('__dirname', '../public') })
   );
 
   app.listen(PORT, function () {
-    console.log("LIBIAM APP is LIVE:", PORT);
+    console.log('LIBIAM APP is LIVE:', PORT);
   });
 };
 
