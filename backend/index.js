5c2a8c4e4bce786dc61990a5da8446ab69f7b6ba
Justin Jang <jangju99@gmail.com>
2021-10-31 12:41:18 -0700

    changed app.use '/' to allow frontend edits visible

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,36 @@
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
-const morgan = require('morgan');
-const path = require('path');
-require('dotenv').config();
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 const initializeExpress = () => {
   const app = express();
 
   if (NODE_ENV === "production") {
     app.use(cors);
-    app.use(morgan('combined'));
+    app.use(morgan("combined"));
   } else {
-    app.use(morgan('dev'));
+    app.use(morgan("dev"));
   }
 
-  app.use('/public', express.static(path.resolve(__dirname, '../public')));
+  app.use("/", express.static(path.resolve(__dirname, "../public")));
 
-  app.get('*', (req, res) => res.sendFile('index.html', { root: path.join('__dirname', '../public') }));
+  app.get("*", (req, res) =>
+    res.sendFile("index.html", { root: path.join("__dirname", "../public") })
+  );
 
-  app.listen(PORT, function() {
-    console.log('LIBIAM APP is LIVE:', PORT)
-  })
-}
+  app.listen(PORT, function () {
+    console.log("LIBIAM APP is LIVE:", PORT);
+  });
+};
 
 const initializeApp = () => {
   initializeExpress();
-}
+};
 
 initializeApp();
