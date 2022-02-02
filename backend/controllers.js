65fd48cdd531792b19c1c0bbca86603398553bc0
KweonMo Koo <mikekoo2010@gmail.com>
2022-02-01 19:30:44 -0800

    Build initial REST endpoints

---
+++ backend/controllers.js
@@ -0,0 +1,47 @@
+const dbHelper = require('./dbHelper.js');
+
+const registrationQ = `INSERT INTO test (name, username, email, password) VALUES (?,?,?,?)`;
+
+module.exports = {
+  getUser: async (req, res) => {
+    res.send('Successfully reached GET User endpoint')
+  },
+  registerUser: async (req, res) => {
+    console.log(req.query)
+    
+    if (req.query) {
+      const {
+        name,
+        username,
+        email,
+        password,
+      } = req.query
+      const userData = [name, username, email, password];
+
+      await dbHelper.queryDB(registrationQ, 'users', userData);
+
+      res.send('REGISTERED');
+    } else {
+      res.send('QUERY PARAMETER IS MISSING')
+    }
+  },
+  updateUser: async (req, res) => {
+    console.log(req.query)
+    res.send(req.query)
+  },
+  authenticateUser: async (req, res) => {
+    res.send(req.query)
+  },
+  getBooks: async (req, res) => {
+    res.send('Successfully reached GET Books endpoint')
+  },
+  personalizeBook: async (req, res) => {
+    res.send(req.query)
+  },
+  generateBook: async (req, res) => {
+    res.send(req.query)
+  },
+  getOrders: async (req, res) => {
+    res.send('Successfully reached GET Orders endpoint')
+  },
+}
